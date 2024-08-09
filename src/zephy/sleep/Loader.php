d112d795d4bfb1bb1d04c43772538718582ff088
Zephyrhz0 <162658447+CosmicDevTeam@users.noreply.github.com>
2024-08-09 10:18:54 -0700

    Update Loader.php

--- src/zephy/sleep/Loader.php
+++ src/zephy/sleep/Loader.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace zephy\sleep;
-
+use pocketmine\utils\SingletonTrait;
 use pocketmine\plugin\PluginBase;
 use zephy\sleep\listener\BedListener;
 class Loader extends PluginBase {
+   use SingletonTrait;
    protected function onEnable(): void {
+     self::setInstance($this);
      $this->getServer()->getPluginManager()->registerEvents(new BedListener(), $this);
    }
 }
